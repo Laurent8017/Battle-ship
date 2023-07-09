bbb360ae30fa7f8c203c93cf1a2e05cef553a9f4
Laurent8017 <>
2023-07-09 16:19:01 +0000

    add basis

--- Main.py
+++ Main.py
@@ -2,7 +2,36 @@ import pygame
 
 
 class Game:
-    def __ init__(self, screen: pygame.Surface):
+    def __init__(self, screen: pygame.Surface):
         self.screen = screen
         self.w, self.h = screen.get_size()
         self.running = True
+        self.clock = pygame.time.Clock()
+
+    def handling_events(self):
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def update(self, dt):
+        pass
+
+    def display(self):
+        self.screen.fill((0, 0, 0))
+
+        pygame.display.flip()
+
+    def run(self):
+        while self.running:
+            self.handling_events()
+            self.update(self.clock.get_time()/1_000)
+            self.display()
+            self.clock.tick()
+
+
+pygame.init()
+win = pygame.display.set_mode((800, 800))
+game = Game(win)
+game.run()
+pygame.quit()
